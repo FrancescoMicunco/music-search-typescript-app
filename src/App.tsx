38c13ca6691fb6094fe10bb88a322006d315dbc6
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-02-02 15:17:03 +0100

    Importing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import{ BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
+  
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
         <h1>Home</h1>
       </header>
     </div>
+    <Routes>
+      <Route path="/" element="<HomePage />"/>
+      <Route path="/detail" element="<DetailsPage />"/>
+      <Route path="*" element="Page Not found!"/>
+    </Routes>
+    
+    </BrowserRouter>
+
+    
   );
 }
 
