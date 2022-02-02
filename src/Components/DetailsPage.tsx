f12ec672f94da703e8a3b6605ef2b251be811267
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-02-02 17:04:50 +0100

    detail

--- src/Components/DetailsPage.tsx
+++ src/Components/DetailsPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { ISongs } from "../Types/SongsType"
 import { useState } from "react"
 import { useParams } from "react-router-dom"
+import { Card } from "react-bootstrap"
 
 export const Details = () =>{
 
@@ -28,6 +29,21 @@ getDetailSong()
     }, [])
 
     return(
+        <>
         <h1>This is the Details Page</h1>
+        detail?.map(d=>
+
+            <Card >
+  <Card.Img variant="top" src="d.album.cover_small" />
+  <Card.Body>
+    <Card.Title>{d.title}</Card.Title>
+    
+    <Button variant="primary">{d.preview}</Button>
+  </Card.Body>
+</Card>
+        )
+        </>
+        
+        
     )
 }
